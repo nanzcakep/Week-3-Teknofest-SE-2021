e2d81dbecd33fa855b4a3fb962fa3c789453ee7c
NR <nr@Nurul.local>
2021-12-13 14:34:39 +0700

    TASK1

---
+++ TASK1/1.py
@@ -0,0 +1,8 @@
+for a in range(0,5):
+    while a <= 4 :
+        print("*",end=" ")
+        a+=1;  
+    print("\r")     
+
+
+     
---
+++ TASK1/5.py
@@ -0,0 +1,10 @@
+for a in range(1,10):
+    for b in range(a,1,-1):
+        print(end=" ")
+    for c in range(a,9+1):
+        print("*",end=" ")    
+    print("\r")    
+   
+
+
+
